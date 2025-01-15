3bdeb52c5d6f2142b72247f1fbad59741e529b72
Lines Reflect <107893733+LinesReflect@users.noreply.github.com>
2025-01-15 15:09:23 -0600

    Add Devise views and fix sign up/in submissions

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   root to: "home#index"
 
   resources :home, only: [ :index ]
-  resources :user, only: [ :new, :create ]
   resources :post, only: [ :new, :create, :index ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
