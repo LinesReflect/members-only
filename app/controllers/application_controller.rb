b85b1467c39296cd56b59b858258e5893cf29198
Lines Reflect <107893733+LinesReflect@users.noreply.github.com>
2025-01-18 20:46:48 -0600

    Refactor code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,12 +7,4 @@ class ApplicationController < ActionController::Base
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-
-  # protected
-
-  # def configure_permitted_parameters
-  #   devise_parameter_sanitizer.permit(:sign_in) do |user_params|
-  #     user_params.permit(:username)
-  #   end
-  # end
 end
